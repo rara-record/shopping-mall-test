46977fcdc7763487b51581ba174066f842c235a8
BORA <a52447879@gmail.com>
2023-05-07 18:59:06 +0900

    feat: CartView.test.tsx test

--- src/components/cart/CartView.test.tsx
+++ src/components/cart/CartView.test.tsx
@@ -1,11 +1,33 @@
+import { screen } from '@testing-library/react';
 import CartView from './CartView';
-
-// test
+import { render } from '../../test-helpers';
+import fixtures from '../../../fixtures';
 
 const context = describe;
 
 describe('CartView', () => {
   // TODO #1: Cart가 비었을 때
+  context('when the cart is empty', () => {
+    const cart = {
+      lineItems: [],
+      totalPrice: 0,
+    };
+
+    it('renders the empty message', () => {
+      render(<CartView cart={cart} />);
+
+      screen.getByText(/비었습니다/);
+    });
+  });
 
   // TODO #2: Cart에 line item이 있을 때
+  context('when the cart has a line item', () => {
+    const { cart } = fixtures;
+
+    it('renders a line item', () => {
+      render(<CartView cart={cart} />);
+
+      screen.getByText(cart.lineItems[0].product.name);
+    });
+  });
 });
