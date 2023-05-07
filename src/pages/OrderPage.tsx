3446646b6470ff1686269b436072074905d6c8e6
BORA <a52447879@gmail.com>
2023-05-07 18:34:30 +0900

    feat:

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,7 +1,5 @@
 import CartView from '../components/cart/CartView';
-
 import useFetchCart from '../hooks/useFetchCart';
-///////
 
 export default function OrderPage() {
   const { cart } = useFetchCart();
